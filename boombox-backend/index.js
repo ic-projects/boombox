61a7aafe6fe5566b2f8652ee552e9b2f3a23e37a
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2017-10-28 18:56:27 +0100

    Write basic song list functionality for API

--- boombox-backend/index.js
+++ boombox-backend/index.js
@@ -21,25 +21,134 @@ let init = async function() {
     console.log('Found parties collection');
   }
 
-  /**
-   * GET Parameters:
-   *  - name: The name of the new party
-   */
   var getCreateParty = async function(req, res) {
     let newPartyId = Math.random().toString(36).substring(6);
     await parties.insertOne({
-      partyId: newPartyId,
-      currentSong: '',
-      currentSongStartTime: 0,
-      nextSong: '',
-      nextSongStartTime: 0,
-      songs: [],
-    });
+      'partyId': newPartyId,
+      'currentSong': '',
+      'currentSongStartTime': 0,
+      'nextSong': '',
+      'nextSongStartTime': 0,
+      'songs': [],
+  	});
     res.send(newPartyId);
+  }
+
+  /**
+   * GET Parameters:
+   *  - partyId
+   */
+  var getSongList = async function(req, res) {
+  	let songList = await parties.findOne({
+  		'partyId': req.query.partyId,
+  	});
+  	res.send(songList);
+  }
+
+  /**
+   * GET Parameters:
+   *  - partyId
+   */
+  var getNextSong = async function(req, res) {
+  	let songList = await parties.findOne({
+  		'partyId': req.query.partyId,
+  	}, {
+  		'fields': { nextSong: 1, nextSongStartTime: 1 }
+  	});
+  	res.send(songList);
+  }
+
+  /**
+   * GET Parameters:
+   *  - partyId
+   *  - songId
+   *  - userId
+   */
+  var getAddSong = async function(req, res) {
+  	// TODO: Don't allow the same song to be added twice?
+
+  	await parties.findAndModify({
+  		'partyId': req.query.partyId,
+  	}, [], {
+  		'$push': { 
+  			'songs': {
+  				'songId': req.query.songId,
+  				'userId': req.query.userId,
+  				'voterIds': [],
+  			}
+  		}
+  	});
+
+  	res.send(true);
+  }
+
+  /**
+   * GET Parameters:
+   *  - partyId
+   *  - songId
+   *  - userId
+   */
+  var getRemoveSong = async function(req, res) {
+  	await parties.findAndModify({
+  		'partyId': req.query.partyId,
+  	}, [], {
+  		'$pull': { 
+  			'songs': {
+  				'songId': req.query.songId,
+  				'userId': req.query.userId,
+  			}
+  		}
+  	});
+
+  	res.send(true);
+  }
+
+  /**
+   * GET Parameters:
+   *  - partyId
+   *  - songId
+   *  - userId
+   */
+  var getVoteSong = async function(req, res) {
+  	await parties.findAndModify({
+  		'partyId': req.query.partyId,
+  		'songs.songId': req.query.songId,
+  	}, [], {
+  		'$push': { 
+  			'songs.$.voterIds': req.query.userId
+  		}
+  	});
+
+  	res.send(true);
+  }
+
+  /**
+   * GET Parameters:
+   *  - partyId
+   *  - songId
+   *  - userId
+   */
+  var getUnvoteSong = async function(req, res) {
+  	await parties.findAndModify({
+  		'partyId': req.query.partyId,
+  		'songs.songId': req.query.songId,
+  	}, [], {
+  		'$pull': { 
+  			'songs.$.voterIds': req.query.userId
+  		}
+  	});
+
+  	res.send(true);
   }
 
   // Handle API calls
   app.get('/createParty', getCreateParty);
+  app.get('/songList', getSongList);
+  app.get('/nextSong', getNextSong);
+  app.get('/addSong', getAddSong);
+  app.get('/removeSong', getRemoveSong);
+  app.get('/voteSong', getVoteSong);
+  app.get('/unvoteSong', getUnvoteSong);
 }
 
 co(init).catch(function(err) {
