7e6fcecedc21c21d49d4f65c9d9983700cecc788
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-10-28 14:27:17 +0100

    Fix code style and require MongoDB

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Why use boombox?
 
 ## Installation
 
-...
+1. Install MongoDB using instructions found [here](https://docs.mongodb.com/manual/tutorial/install-mongodb-enterprise-on-ubuntu/#install-mongodb-enterprise-from-tarball).
 
 
 ## Usage
--- boombox-backend/index.js
+++ boombox-backend/index.js
@@ -1,10 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
 
 });
