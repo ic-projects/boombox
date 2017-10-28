8e6fc0597cbe8611c4fc1c2136f33bae45f71ee8
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-10-28 16:23:37 +0100

    Connect to MongoDB and create parties collection

--- boombox-backend/index.js
+++ boombox-backend/index.js
@@ -1,21 +1,49 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var MongoClient = require('mongodb').MongoClient;
+var co = require('co');
 var assert = require('assert');
 
-// Setup MongoDB database
-var url = 'mongodb://localhost:27017/boombox';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+let init = async function() {
+  // Setup MongoDB database
+  var db = await MongoClient.connect('mongodb://localhost:27017/boombox');
+  console.log('Connected correctly to server');
 
-  db.close();
+  // Create parties collection if needed
+  let partiesCollections = await db.listCollections({
+    name: 'parties'
+  }).toArray();
+  if (partiesCollections.length == 0) {
+    var parties = await db.createCollection('parties');
+    console.log('Created parties collection');
+  } else {
+    var parties = db.collection('parties');
+    console.log('Found parties collection');
+  }
+
+  /**
+   * GET Parameters:
+   *  - name: The name of the new party
+   */
+  var getCreateParty = async function(req, res) {
+    let newPartyId = Math.random().toString(36).substring(6);
+    await parties.insertOne({
+      partyId: newPartyId,
+      currentSong: '',
+      currentSongStartTime: 0,
+      nextSong: '',
+      nextSongStartTime: 0,
+      songs: [],
+    });
+    res.send(newPartyId);
+  }
+
+  // Handle API calls
+  app.get('/createParty', getCreateParty);
+}
+
+co(init).catch(function(err) {
+  console.log(err.stack);
 });
 
-app.get('/', function(req, res) {
-
-});
-
-http.listen(3000, function() {
-
-});
+http.listen(30000);
