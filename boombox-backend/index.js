153fac6fa697fb07cfb380ff5de23802c1b608bb
Qiang Feng <Qiang.Feng@hotmail.co.uk>
2017-10-28 14:30:02 +0100

    Setup MongoDB connection

--- boombox-backend/index.js
+++ boombox-backend/index.js
@@ -3,6 +3,15 @@ var http = require('http').Server(app);
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
+// Setup MongoDB database
+var url = 'mongodb://localhost:27017/boombox';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
 app.get('/', function(req, res) {
 
 });
