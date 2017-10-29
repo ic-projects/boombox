8983a605966c0bba0bb4a7153a9eca07d8683a68
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2017-10-29 12:36:12 +0000

    Implement basic YT search functionality

---
+++ boombox-frontend/src/components/SearchElement.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="list-group-item"
+      @click="addSong">
+    {{ songTitle }}
+  </li>
+</template>
+<script>
+  var he = require('he');
+export default {
+  name: 'SearchElement',
+  props: ['songId', 'songTitle'],
+  methods: {
+    addSong () {
+      this.$socket.emit('addSong', { partyId: this.partyId, songId: this.songId, userId: this.uuid })
+    }
+  }
+}
+</script>
+<style>
+ul li:hover {
+  cursor: pointer;
+}
+</style>
